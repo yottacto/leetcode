5c2e29a21e2ac783508d52c55a45cfd495e663ff
yottacto <yotta.w.cto@gmail.com>
2017-12-21 10:22:23 +0800

    [@] 7, modern init style

--- problems/7-reverse-integer/7.hh
+++ problems/7-reverse-integer/7.hh
@@ -8,8 +8,8 @@ public:
 
     int reverse(int x)
     {
-        long long ret = 0;
-        for (long long a = x; a; a /= 10)
+        long long ret{0};
+        for (long long a{x}; a; a /= 10)
             ret = ret * 10 + (a % 10);
         if (int(ret) != ret) ret = 0;
         return ret;
