8e8a095e93e9abdd9936eeced326e9e03d64c4b6
yottacto <yotta.w.cto@gmail.com>
2017-12-23 11:55:17 +0800

    [@] a more concise solution

--- problems/66-plus-one/66.hh
+++ problems/66-plus-one/66.hh
@@ -6,16 +6,29 @@
 class Solution
 {
 public:
+    // std::vector<int> plusOne(std::vector<int>& digits)
+    // {
+    //     if (std::all_of(digits.begin(), digits.end(), [](int i) { return i == 9; }))
+    //         digits.insert(digits.begin(), 0);
+    //     *digits.rbegin() += 1;
+    //     for (auto it = digits.rbegin(); it != digits.rend(); ++it) {
+    //         if (*it < 10) break;
+    //         (*std::next(it))++;
+    //         *it = 0;
+    //     }
+    //     return digits;
+    // }
+
     std::vector<int> plusOne(std::vector<int>& digits)
     {
-        if (std::all_of(digits.begin(), digits.end(), [](int i) { return i == 9; }))
-            digits.insert(digits.begin(), 0);
-        *digits.rbegin() += 1;
-        for (auto it = digits.rbegin(); it != digits.rend(); ++it) {
-            if (*it < 10) break;
-            (*std::next(it))++;
-            *it = 0;
-        }
+        for (auto it = digits.rbegin(); it != digits.rend(); ++it)
+            if (*it != 9) {
+                (*it)++;
+                return digits;
+            } else
+                *it = 0;
+        digits[0] = 1;
+        digits.emplace_back(0);
         return digits;
     }
 };
