cc9ab265277251da3fa5cdb01d5044e6ffae9a36
yottacto <yotta.w.cto@gmail.com>
2018-12-03 13:53:19 +0800

    Modify 795, a more concise solution

--- problems/795-number-of-subarrays-with-bounded-maximum/795.hh
+++ problems/795-number-of-subarrays-with-bounded-maximum/795.hh
@@ -1,37 +1,20 @@
+// more concise solution
 #pragma once
 #include <vector>
 #include <deque>
 
-using ll = long long;
-
 struct Solution
 {
-    auto inrange(int x, int l, int r)
+    int numSubarrayBoundedMax(std::vector<int> const& a, int l, int r)
     {
-        return l <= x && x <= r;
-    }
-
-    ll numSubarrayBoundedMax(std::vector<int> const& a, int l, int r)
-    {
-        auto last = -1;
+        auto left = -1;
+        auto right = -1;
         int n = a.size();
-        std::deque<int> dq;
-        auto res = 0ll;
+        auto res = 0;
         for (auto i = 0; i < n; i++) {
-            while (!dq.empty() && a[i] >= a[dq.back()])
-                dq.pop_back();
-            dq.push_back(i);
-            while (!dq.empty() && !inrange(a[dq.front()], l, r)) {
-                if (a[dq.front()] > r)
-                    last = dq.front();
-                dq.pop_front();
-            }
-            while (!dq.empty() && !inrange(a[dq.back()], l, r))
-                dq.pop_back();
-            auto right = last;
-            if (!dq.empty())
-                right = dq.back();
-            res += right - last;
+            if (a[i] > r) left = i;
+            if (a[i] >= l) right = i;
+            res += right - left;
         }
         return res;
     }
