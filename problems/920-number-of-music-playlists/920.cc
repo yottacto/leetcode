052d149461032c688a9602abdc266958a0ffa50f
yottacto <yotta.w.cto@gmail.com>
2018-12-08 01:08:13 +0800

    Add 920, dp

---
+++ problems/920-number-of-music-playlists/920.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "920.hh"
+
+int main()
+{
+    std::cout << Solution{}.numMusicPlaylists(3, 3, 1) << "\n";
+    std::cout << Solution{}.numMusicPlaylists(2, 3, 0) << "\n";
+    std::cout << Solution{}.numMusicPlaylists(2, 3, 1) << "\n";
+}
+
---
+++ problems/920-number-of-music-playlists/920.hh
@@ -0,0 +1,31 @@
+#pragma once
+#include <algorithm>
+
+#include <iostream>
+
+using ll = long long;
+
+auto constexpr maxn = 200;
+auto constexpr mo = 1000000007ll;
+
+ll add(ll x, ll y) { return (x + y) % mo; }
+ll mul(ll x, ll y) { return (x * y) % mo; }
+
+struct Solution
+{
+    int f[maxn][maxn] = {};
+
+    int numMusicPlaylists(int n, int l, int k)
+    {
+        f[1][1] = n;
+        for (auto i = 1; i <= l; i++) {
+            for (auto j = 1; j <= std::min(i, n); j++) {
+                if (j >= k + 1)
+                    f[i+1][j] = add(f[i+1][j], mul(f[i][j], j - k));
+                f[i+1][j+1] = add(f[i+1][j+1], mul(f[i][j], n - j));
+            }
+        }
+        return f[l][n];
+    }
+};
+
