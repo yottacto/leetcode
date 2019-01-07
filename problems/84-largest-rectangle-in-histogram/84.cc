20eee06ed5f87d9f47291836f25d3a15836dae90
yottacto <yotta.w.cto@gmail.com>
2019-01-08 00:22:29 +0800

    Add 84, monotonic queue

---
+++ problems/84-largest-rectangle-in-histogram/84.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "84.hh"
+
+int main()
+{
+    std::cout << Solution{}.largestRectangleArea({10, 2, 2, 2, 2, 10}) << "\n";
+}
+
---
+++ problems/84-largest-rectangle-in-histogram/84.hh
@@ -0,0 +1,48 @@
+#pragma once
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+struct Solution
+{
+    int n;
+    std::vector<int> left;
+    std::vector<int> right;
+
+    void calc(std::vector<int> const& h, std::vector<int>& d, bool left = true)
+    {
+        d.resize(n);
+        std::deque<int> dq;
+        for (auto i = left ? 0 : n - 1; left ? i < n : i >= 0; left ? i++ : i--) {
+            while (!dq.empty() && h[dq.back()] >= h[i])
+                dq.pop_back();
+            d[i] = dq.empty()
+                ? (left ? -1 : n)
+                : dq.back();
+            dq.emplace_back(i);
+        }
+
+    }
+
+    int largestRectangleArea(std::vector<int> const& heights)
+    {
+        n = heights.size();
+        calc(heights, left);
+        calc(heights, right, false);
+        auto max = 0;
+        for (auto i = 0; i < n; i++)
+            max = std::max(max, (right[i] - left[i] - 1) * heights[i]);
+        // print(left);
+        // print(right);
+        return max;
+    }
+
+    template <class Vec>
+    void print(Vec const& v)
+    {
+        for (auto i : v)
+            std::cout << i << ", ";
+        std::cout << "\n";
+    }
+};
+
