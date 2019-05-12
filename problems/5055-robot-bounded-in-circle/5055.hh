1b05781ff010f9d3453ffeff04fa9438c32936f0
yottacto <yotta.w.cto@gmail.com>
2019-05-12 13:50:29 +0800

    Simplify 5055

--- problems/5055-robot-bounded-in-circle/5055.hh
+++ problems/5055-robot-bounded-in-circle/5055.hh
@@ -15,27 +15,23 @@ struct Solution
     {
         auto d = 0;
         auto cur = std::make_pair(0, 0);
-        for (auto i = 0; i <= 100; i++) {
-            for (auto ch : instructions) {
-                if (ch == 'L') {
-                    d--;
-                    if (d == -1)
-                        d = 3;
-                } else if (ch == 'R') {
-                    d++;
-                    if (d == 4)
-                        d = 0;
-                } else {
-                    auto next = cur;
-                    next.first += dir[d][0];
-                    next.second += dir[d][1];
-                    cur = next;
-                }
+        for (auto ch : instructions) {
+            if (ch == 'L') {
+                d--;
+                if (d == -1)
+                    d = 3;
+            } else if (ch == 'R') {
+                d++;
+                if (d == 4)
+                    d = 0;
+            } else {
+                auto next = cur;
+                next.first += dir[d][0];
+                next.second += dir[d][1];
+                cur = next;
             }
-            if (cur == std::make_pair(0, 0) && d == 0)
-                return true;
         }
-        return false;
+        return d != 0 || cur == std::make_pair(0, 0);
     }
 };
 
