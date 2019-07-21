84e1224e94a11ba3859d19142bf0423c6d4c8a3a
yottacto <yotta.w.cto@gmail.com>
2019-07-21 14:55:13 +0800

    Add 395, two pointer solution, actually slower than previous one

--- problems/395-longest-substring-with-at-least-k-repeating-characters/395.hh
+++ problems/395-longest-substring-with-at-least-k-repeating-characters/395.hh
@@ -2,29 +2,71 @@
 #include <vector>
 #include <string>
 
+// O(alpha * n), two pointer
 struct Solution
 {
     int longestSubstring(std::string s, int k)
     {
-        std::vector<int> count(26);
-        for (auto ch : s)
-            count[ch - 'a']++;
         int n = s.size();
+        std::unordered_map<char, int> count;
         auto res = 0;
-        auto last = 0;
-        for (auto i = 0; i < n; i++) {
-            if (count[s[i] - 'a'] < k) {
-                res = std::max(res, longestSubstring(s.substr(last, i - last), k));
-                last = i + 1;
+        for (auto unique = 1; unique <= 26; unique++) {
+            auto j = 0;
+            int tot_diff = 0;
+            int reach_k = 0;
+            for (auto i = 0; i < n; i++) {
+                while (
+                    j < n && (
+                        tot_diff < unique
+                        || (tot_diff == unique && count.count(s[j]))
+                    )
+                ) {
+                    if (!count.count(s[j]))
+                        tot_diff++;
+                    count[s[j]]++;
+                    if (count[s[j]] == k)
+                        reach_k++;
+                    j++;
+                }
+                if (reach_k == tot_diff)
+                    res = std::max(res, j - i);
+                if (count[s[i]] == k)
+                    reach_k--;
+                count[s[i]]--;
+                if (!count[s[i]]) {
+                    tot_diff--;
+                    count.erase(s[i]);
+                }
             }
-        }
-        if (last < n) {
-            if (!last)
-                res = n;
-            else
-                res = std::max(res, longestSubstring(s.substr(last, n - last), k));
         }
         return res;
     }
 };
+
+// O(nlogn)
+// struct Solution
+// {
+//     int longestSubstring(std::string s, int k)
+//     {
+//         std::vector<int> count(26);
+//         for (auto ch : s)
+//             count[ch - 'a']++;
+//         int n = s.size();
+//         auto res = 0;
+//         auto last = 0;
+//         for (auto i = 0; i < n; i++) {
+//             if (count[s[i] - 'a'] < k) {
+//                 res = std::max(res, longestSubstring(s.substr(last, i - last), k));
+//                 last = i + 1;
+//             }
+//         }
+//         if (last < n) {
+//             if (!last)
+//                 res = n;
+//             else
+//                 res = std::max(res, longestSubstring(s.substr(last, n - last), k));
+//         }
+//         return res;
+//     }
+// };
 
