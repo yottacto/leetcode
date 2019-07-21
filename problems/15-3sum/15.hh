ccb91deb93b936f1ee767635274739e28129374d
yottacto <yotta.w.cto@gmail.com>
2019-07-22 00:08:54 +0800

    Modify 15

--- problems/15-3sum/15.hh
+++ problems/15-3sum/15.hh
@@ -11,21 +11,25 @@ struct Solution
         for (auto i : nums)
             count[i]++;
         std::vector<std::vector<int>> res;
-        for (auto it = count.begin(); it != count.end(); ++it) {
-            if (it->second >= 2) {
-                auto sum = 2 * it->first;
-                if (-sum != it->first && count.count(-sum))
+        std::vector<std::pair<int, int>> all;
+        all.resize(count.size());
+        auto n = 0;
+        for (auto const& p : count)
+            all[n++] = p;
+        for (auto const& p : all) {
+            if (p.second >= 2) {
+                auto other = -2 * p.first;
+                if (other != p.first && count.count(other))
                     res.emplace_back(std::initializer_list<int>({
-                        it->first, it->first, -sum
+                        p.first, p.first, other
                     }));
             }
-            for (auto it2 = count.begin(); it2 != count.end(); ++it2) {
-                auto sum = it->first + it2->first;
-                if (it->first >= it2->first || it2->first >= -sum || !count.count(-sum))
-                    continue;
-                res.emplace_back(std::initializer_list<int>({
-                    it->first, it2->first, -sum
-                }));
+            for (auto const& p2 : all) {
+                auto other = -(p.first + p2.first);
+                if (p.first < p2.first && p2.first < other && count.count(other))
+                    res.emplace_back(std::initializer_list<int>({
+                        p.first, p2.first, other
+                    }));
             }
         }
         if (count.count(0) && count.at(0) >= 3)
