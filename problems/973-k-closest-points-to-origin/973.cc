69c1dabff4f3ab6694155c158fdf117354c4ef57
yottacto <yotta.w.cto@gmail.com>
2019-09-24 10:36:51 +0800

    Add 973, k largest linear algorithm, quick select

--- problems/973-k-closest-points-to-origin/973.cc
+++ problems/973-k-closest-points-to-origin/973.cc
@@ -1,5 +1,29 @@
+#include <iostream>
 #include "973.hh"
+
+template <class Vec>
+void print(Vec const& v)
+{
+    for (auto i : v)
+        std::cout << i[0] << " " << i[1] << "\n";
+}
 
 int main()
 {
+
+    {
+        std::vector<std::vector<int>> points{
+            {1, 3}, {-2, 2},
+        };
+        print(Solution{}.kClosest(points, 1));
+    }
+
+    {
+        std::vector<std::vector<int>> points{
+            {3, 3}, {5, -1}, {-2, 4},
+        };
+        print(Solution{}.kClosest(points, 2));
+    }
+
 }
+
--- problems/973-k-closest-points-to-origin/973.hh
+++ problems/973-k-closest-points-to-origin/973.hh
@@ -1,28 +1,56 @@
 #include <vector>
+#include <algorithm>
 #include <queue>
+#include <random>
 
 struct Solution
 {
-    int dis(std::vector<int> const& p)
+    using point_type = std::vector<int>;
+
+    std::random_device rd{};
+    std::mt19937 gen{rd()};
+
+    int distance(std::vector<int> const& p)
     {
         return p[0] * p[0] + p[1] * p[1];
     }
 
-    std::vector<std::vector<int>> kClosest(std::vector<std::vector<int>>& points, int k)
+    void quick(std::vector<point_type>& points, int start, int end, int k)
+    {
+        if (k == end - start)
+            return;
+        std::uniform_int_distribution<> dis(start, end - 1);
+        // auto pivot = distance(points[start]);
+        auto pivot = distance(points[dis(gen)]);
+        auto i = start;
+        auto j = end - 1;
+        while (i <= j) {
+            while (distance(points[i]) < pivot)
+                i++;
+            while (distance(points[j]) > pivot)
+                j--;
+            if (i <= j) {
+                std::swap(points[i], points[j]);
+                i++;
+                j--;
+            }
+        }
+        auto left = i - start;
+        if (k <= left) {
+            quick(points, start, i, k);
+        } else {
+            quick(points, i, end, k - left);
+        }
+    }
+
+    std::vector<point_type> kClosest(std::vector<point_type>& points, int k)
     {
         int n = points.size();
-        auto cmp = [&](auto i, auto j) {
-            return dis(points[i]) < dis(points[j]);
-        };
-        std::priority_queue<int, std::vector<int>, decltype(cmp)> pq{cmp};
-        for (auto i = 0; i < n; i++) {
-            pq.emplace(i);
-            if (static_cast<int>(pq.size()) > k)
-                pq.pop();
-        }
-        std::vector<std::vector<int>> res;
-        for (; !pq.empty(); pq.pop())
-            res.emplace_back(points[pq.top()]);
+        quick(points, 0, n, k);
+        std::vector<point_type> res;
+        res.reserve(k);
+        for (auto i = 0; i < k; i++)
+            res.emplace_back(points[i]);
         return res;
     }
 };
